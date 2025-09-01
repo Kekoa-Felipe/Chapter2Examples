e1776d68c71b961788172204b498b5b5aff6f1c5
Kekoa-Felipe <kf29521@email.vccs.edu>
2025-08-31 21:37:39 -0400

    Added code to accept and display the first name.

--- Chapter2Examples.cpp
+++ Chapter2Examples.cpp
@@ -1,13 +1,14 @@
 /* Program File Name: Chapter2Examples
-Programmer: Kekoa Felipe
-Date: August 2025
-Requirements:
-Write a program that has the following character variables:
-first, middle, and last. Store your initials in these variables
-then display them on the screen.
+   Programmer: Kekoa Felipe
+   Date: August 2025
+   Requirements:
+   Write a program that has the following character variables:
+   first, middle, and last. Store your initials in these variables
+   then display them on the screen.
 */
 
 #include <iostream>
+#include <string>
 
 
 
@@ -16,12 +17,17 @@ int main()
     char firstInitial;
     char middleInitial;
     char lastInitial;
+    std::string firstName;
     std::cout << "Please enter the initial of your first name:";
     std::cin >> firstInitial;
     std::cout << "Please enter the initial of your middle name:";
     std::cin >> middleInitial;
     std::cout << "Please enter the initial of your last name:";
     std::cin >> lastInitial;
-    std::cout << "Your initals are: " << firstInitial << middleInitial << lastInitial;
-}
+    std::cout << "Your initals are: " << firstInitial << middleInitial << lastInitial << std::endl;
+    std::cout << "Please enter your first name:";
+    std::cin >> firstName;
+    std::cout << "Your first name is : " << firstName;
 
+
+}
